45ae85f8caf569f712a76c7c790c22d6d0fca591
ahmadjaara <ahmadjaara98@yahoo.com>
2022-01-26 08:46:21 +0200

    employee render

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-function hrMangment (id,fullName,department,level){
+function hrMangment (id,fullName,img,department,level){
     this.id=id;
     this.fullName=fullName;
-    this.img=`./imgs/${this.fullName}.jpg`;
+    this.img= img;
     this.department=department;
     this.level=level
-}
+};
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
-
-const employ0 = new hrMangment(1000,"Ghazi Samer",["Administration"],["Senior"]);
-const employ1 = new hrMangment(1001,"Lana Ali",["Finance"],["Senior"]);
-const employ2 = new hrMangment(1002,"Tamara Ayoub",["Marketing"],["Senior"]);
-const employ3 = new hrMangment(1003,"Safi Walid",["Administration"],["Mid-Senior"]);
-const employ4 = new hrMangment(1004,"Omar Zaid",["Development"],["Senior"]);
-const employ5 = new hrMangment(1005,"Rana Saleh",["Development"],["Junior"]);
-const employ6 = new hrMangment(1006,"Hadi Ahmad",["Finance"],["Mid-Senior"]);
+  };
+//"/home/ahmad/HR-management-system/imgs"
+const employ0 = new hrMangment(1000,"Ghazi Samer",`./imgs/${this.fullName}.jpg`,["Administration"],["Senior"]);
+const employ1 = new hrMangment(1001,"Lana Ali",`./imgs/${this.fullName}.jpg`,["Finance"],["Senior"]);
+const employ2 = new hrMangment(1002,"Tamara Ayoub",`./imgs/${this.fullName}.jpg`,["Marketing"],["Senior"]);
+const employ3 = new hrMangment(1003,"Safi Walid",`./imgs/${this.fullName}.jpg`,["Administration"],["Mid-Senior"]);
+const employ4 = new hrMangment(1004,"Omar Zaid",`./imgs/${this.fullName}.jpg`,["Development"],["Senior"]);
+const employ5 = new hrMangment(1005,"Rana Saleh",`./imgs/${this.fullName}.jpg`,["Development"],["Junior"]);
+const employ6 = new hrMangment(1006,"Hadi Ahmad",`./imgs/${this.fullName}.jpg`,["Finance"],["Mid-Senior"]);
 
 hrMangment.prototype.Salary=function(){
 
@@ -60,9 +60,32 @@ employ4.Salary();
 employ5.Salary();
 employ6.Salary();
 
-
 hrMangment.prototype.render =function(){
-    document.write(`<p><h2>"${this.fullName}" is an employee in our companey and its salary is : ${this.Salary()} $$</h2> </p>`)
+    //document.write(`<p><h2>"${this.fullName}" is an employee in our companey and its salary is : ${this.Salary()} $$</h2> </p>`)
+   let mysection =document.getElementById("hi");
+  
+   let divElem1 = document.createElement('div');
+  
+   mysection.appendChild(divElem1);
+
+   let img =document.createElement('img');
+   img.appendChild(divElem1);
+   img.setAttribute('src',this.img);
+
+
+   let pEle =document.createElement('p');
+   divElem1.append(pEle);
+   pEle.textContent=`name:${this.name} -ID:${this.id}`;
+
+   let pEle2=document.createElement('p')
+   divElem1.append(pEle2);
+   pEle2.textContent=`Department:${this.department}-Level:${this.level}`;
+
+   let pEle3=document.createElement('p');
+   divElem1.appendChild(pEle3);
+   pEle3.textContent=`${this.Salary()}`;
+
+
 }
 
 employ0.render();
@@ -72,3 +95,28 @@ employ3.render();
 employ4.render();
 employ5.render();
 employ6.render();
+
+////html event 
+function func(){
+    let d =new Date().getTime().toString();
+    let id = d.slice(d.length-4);
+    return id;
+}
+let addEmploy = document.getElementById("addEmploy");
+
+addEmploy.addEventListener('submit',addEmployInfo);
+
+function addEmployInfo (event){
+    event.preventDefault();
+    let FullName =event.target.fullName.value;
+    let Department=event.target.select.value;
+    let Image=event.target.Imageurl.value ;
+    let level =event.target.selectLe.value;
+    console.log(FullName,Department,Image);
+
+
+    const employee = new hrMangment(func(),FullName,Image,Department,level);
+    employee.Salary();
+    employee.render();
+    console.log(employee);
+}
